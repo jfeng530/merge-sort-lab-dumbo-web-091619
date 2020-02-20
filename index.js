6afcab9dcb9ceeb403acbcd84b7f85ed383f355f
Jacky Feng <jackyfeng530@yahoo.com>
2020-02-20 03:14:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
-function findMinAndRemoveSorted (arr1, arr2) {
-  // let min = arr[0]
-  // let index
-  // for (let i = 0; i < arr.length; i++) {
-  //   if (arr[i] < min) {
-  //     min = arr[i]
-  //     index = i
-  //   }
-  // }
-  // arr.splice(index, 1)
-  // return min
-  let min1 = arr1[0]
-  let min2 = arr2[0]
-  
-  if (min1 < min2) {
-    return arr1.shift()
-  } else {
-    return arr2.shift()
-  }
+function findMinAndRemoveSorted (arr) {
+  return arr.shift()
 }
 
 function merge (arr1, arr2) {
