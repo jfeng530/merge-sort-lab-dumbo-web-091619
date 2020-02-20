272b42b0d421721bf46bcf4f1ee613c677373466
Jacky Feng <jackyfeng530@yahoo.com>
2020-02-20 03:17:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,21 @@ function mergeSort (arr) {
   if (arr.length < 2) {
     return arr
   } else {
-    merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf))
   }
+}
+
+
+function mergeSort(array){
+  let midpoint = array.length/2
+  let firstHalf = array.slice(0, midpoint)
+  let secondHalf = array.slice(midpoint, array.length)
+  let sorted;
+
+  if(array.length < 2){
+    return array
+  } else {
+    sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  }
+  return sorted
 }
