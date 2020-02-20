ff9bead11607575f89dc3e9f792fc46fcfdd2182
Jacky Feng <jackyfeng530@yahoo.com>
2020-02-20 03:06:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function findMinAndRemoveSorted (arr) {
-  
+  let min = arr[0]
+  let index
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i]
+      index = i
+    }
+  }
 }
 
 function merge (arr) {
